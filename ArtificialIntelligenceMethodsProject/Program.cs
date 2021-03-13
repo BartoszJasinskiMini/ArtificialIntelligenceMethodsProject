f1da3e6daa7aa753adf52be633dda2d1309ba891
Bartosz Jasinski <bjasinski@ittouch.pl>
2021-03-13 09:49:02 +0100

    Initial commit

---
+++ ArtificialIntelligenceMethodsProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArtificialIntelligenceMethodsProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
